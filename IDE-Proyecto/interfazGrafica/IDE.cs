7b7ba3d373c3432038671fa3cf9dfeab772e627c
froi0409 <ferrubocana@gmail.com>
2020-09-12 02:27:39 -0600

    Se han guardado cambios relacionados a algunos eventos

--- IDE-Proyecto/interfazGrafica/IDE.cs
+++ IDE-Proyecto/interfazGrafica/IDE.cs
@@ -36,21 +36,43 @@ namespace IDE_Proyecto.interfazGrafica
 
         private void richTextBox2_TextChanged(object sender, EventArgs e)
         {
+
+
             int cantLineasRT = richTextBox2.Lines.Length;
-            if(cantLineasRT != cantLineas)
+            if (cantLineasRT != cantLineas)
             {
                 cantLineas = cantLineasRT;
                 richTextBox1.Clear();
-                for(int i = 1; i <= cantLineasRT; i++)
+                for (int i = 1; i <= cantLineasRT; i++)
                 {
                     richTextBox1.AppendText(i + "\n");
                 }
             }
+
+            //// create & set Point pt to (0,0)    
+            //Point pt = new Point(0, 0);
+            //// get First Index & First Line from richTextBox1    
+            //int First_Index = richTextBox2.GetCharIndexFromPosition(pt);
+            //int First_Line = richTextBox2.GetLineFromCharIndex(First_Index);
+            //// set X & Y coordinates of Point pt to ClientRectangle Width & Height respectively    
+            //pt.X = ClientRectangle.Width;
+            //pt.Y = ClientRectangle.Height;
+            //// get Last Index & Last Line from richTextBox1    
+            //int Last_Index = richTextBox2.GetCharIndexFromPosition(pt);
+            //int Last_Line = richTextBox2.GetLineFromCharIndex(Last_Index);
+            
+            //// set LineNumberTextBox text to null
+            //richTextBox1.Text = "";
+            //// now add each line number to LineNumberTextBox upto last line    
+            //for (int i = First_Line; i <= Last_Line; i++)
+            //{
+            //    richTextBox1.Text += i + 1 + "\n";
+            //}
         }
 
         private void button6_Click(object sender, EventArgs e)
         {
-            MessageBox.Show(richTextBox2.SelectionStart.ToString());
+            
         }
     }
 }
